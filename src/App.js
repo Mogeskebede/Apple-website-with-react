b53b6b57cb47d1c12cc4e71bf99db7a2dca5fdf3
Moges Kebedew <mogeshailu381@gmail.com>
2022-11-16 17:04:05 -0500

    COMMITED

--- src/App.js
+++ src/App.js
@@ -2,25 +2,13 @@ import React from 'react'
 import Header from './compenents/Header'
 import "./compenents/css/bootstrap.css"
 import './compenents/css/styles.css'
-import FirstSection from './compenents/FirstSection'
-import SecondSection from './compenents/SecondSection'
-import ThirdSection from './compenents/ThirdSection'
-import ForthSection from './compenents/ForthSection'
-import FifthSection from './compenents/FifthSection'
-import SixthSection from './compenents/SixthSection'
-import SeventhSection from './compenents/SeventhSection'
 import Footer from './compenents/Footer'
+import Main from './compenents/Main'
 function App() {
   return (
     <div>
       <Header />
-      <FirstSection />
-      <SecondSection />
-      <ThirdSection />
-      <ForthSection />
-      <FifthSection />
-      <SixthSection />
-      <SeventhSection />
+     <Main/>
       <Footer />
     </div>
   );
---
+++ src/compenents/Main.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import FirstSection from './FirstSection'
+import SecondSection from './SecondSection'
+import ThirdSection from './ThirdSection'
+import ForthSection from './ForthSection'
+import FifthSection from './FifthSection'
+import SixthSection from './SixthSection'
+import SeventhSection from './SeventhSection'
+function Main() {
+  return (
+    <div>
+       <FirstSection />
+      <SecondSection />
+      <ThirdSection />
+      <ForthSection />
+      <FifthSection />
+      <SixthSection />
+      <SeventhSection />
+    </div>
+  )
+}
+
+export default Main
